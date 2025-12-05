baf2fa5ad08512dd25f674c9196ee6481ed89beb
Deepak Kiran <79005966+deepak-kiran@users.noreply.github.com>
2025-12-05 11:10:13 +0530

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,9 +1,8 @@
 import os
 import csv
 import sys
+from pathlib import Path
 
-#⚠️This is the path of the CSV file. Kindly UPDATE it to your CSV file's path for the program to function properly
-CSV_PATH = "D:/#DEEPAK/VS_Project/Hex.csv" 
 
 # Converts hex to right format (8e30123t to 8E 30 12 3T)
 def normalize_hex(s: str) -> str:
@@ -24,20 +23,37 @@ def get_header_trailer(path):
         sys.exit(1)
 
 # Prints the Detected Extension and returns it
-def print_extension(ext_path):
+def validate_extension(ext_path):
     _, ext = os.path.splitext(ext_path)
     ext = ext.lstrip('.').upper()
     print(f"\nGiven File extension: {ext}\n")
+    plain_txt_files = ["TXT","CSV","LOG"]
+    markup_lang = ["HTML","XML","JSON"]
+    config_files = ["INI","CFG","YAML"]
+    genric_scrips = ["PY","SH","BAT","PHP"]
+    if ext in plain_txt_files:
+        print("Given file is a plain text file(.txt, .csv, .log) which has no standard Magic Bytes to verify\n")
+        sys.exit(1)
+    if ext in markup_lang:
+        print("Given file is a text-based formatted (.html, .xml, .json) which has no standard Magic Bytes to verify\n")
+        sys.exit(1)
+    if ext in config_files:
+        print("Given file is a plain text and highly variable in their header bytes (.ini, .cfg, .yaml) which has no standard Magic Bytes to verify\n")
+        sys.exit(1)
+    if ext in genric_scrips:
+        print("Given file is a script file (.py, .sh, .bat, .php) which has no standard Magic Bytes to verify\n")
+        sys.exit(1)
     return ext
 
 # Gets all the matching headers that starts with our file signature and returns the largest matching header
-def get_main_header(key):
+def get_main_header(key,CSV_PATH):
     temp_header_list = []
     with open(CSV_PATH, newline="", encoding="utf-8", errors="ignore") as f:
         reader = csv.DictReader(f)
         for row in reader:
             if key.startswith(row["Header"]):
                 temp_header_list.append(row["Header"])
+    if temp_header_list:    
         return max(temp_header_list)
 
 def main():
@@ -59,6 +75,14 @@ def main():
     best_header = []
     description = []  
     
+    #Determine the directory where the current Python script is located.
+    script_dir = Path(__file__).parent
+
+    #Constructs the path for Hex.csv file combining it with the Python script's directory path
+    CSV_PATH = script_dir / "Hex.csv"
+
+
+
     #Takes file path as input and removes quotes ("",'')
     file_path = input("Please enter the File's path you want to check :- ")
     file_path = file_path.replace('"','').replace("'","")
@@ -66,8 +90,8 @@ def main():
     header,trailer = get_header_trailer(file_path)
     key = normalize_hex(header)
     key2 = normalize_hex(trailer)
-    ext = print_extension(file_path)
-    best_header = get_main_header(key)
+    ext = validate_extension(file_path)
+    best_header = get_main_header(key,CSV_PATH)
 
     # Check in CSV file for right header and store all matching header's Description
     if best_header:
